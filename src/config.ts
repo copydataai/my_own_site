450e369e1b745d5467cc9899c443aadb40addb9c
José Sánchez <copydataai@truora.com>
2023-03-08 22:06:21 +0000

    Add(Feat): Add main page

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const HOMEPAGE_URL = "copydataai.com";
+export const SITE_TITLE = "Jose Sanchez";
+export const SITE_DESCRIPTION = "A website about Jose Sanchez aka copydataai, Full-stack Developer";
